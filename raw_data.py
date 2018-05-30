696df28595a71cc802ad8ee259b99019102e306d
lab-pc1 <cbc106008@nptu.edu.tw>
2018-05-30 22:09:49 +0800

    add capture raw data.

---
+++ raw_data.py
@@ -0,0 +1,14 @@
+import time
+
+from emokit.emotiv import Emotiv
+
+if __name__ == "__main__":
+    with Emotiv(display_output=True, verbose=True) as headset:
+        while True:
+            t2 = time.time()
+            packet = headset.dequeue()
+            if packet is not None:
+                # print(packet.sensors)
+                # print str(time.time() - t2) + "s"
+                # pass
+            time.sleep(0.001)
